4212a93a604dfe3c2089bceeb8047ed8d1454daa
Jake Bogerd <jake.bogerd@inin.com>
2015-01-05 15:03:44 -0500

    AR-1929: Correct cookbook name in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'inin-opsworks'
+name             'ansible-opsworks'
 maintainer       'Interactive Intelligence'
 maintainer_email 'MAINTAINER EMAIL HERE'
 license          'All rights reserved'
-description      'Installs/Configures inin-opsworks'
+description      'Installs/Configures ansible-opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
